4c65d70f36426956a988b66af211ebe4f4c17883
viet tu <quyen.luminous@gmail.com>
2024-05-21 18:55:51 +0700

    (+) constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const Safe4337ModuleAddress = '0xa581c4A4DB7175302464fF3C06380BC3270b4037'
+export const SafeProxyFactoryAddress = '0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67'
+export const SafeSingletonAddress = '0x41675C099F32341bf84BFc5382aF534df5C7461a'
+export const AlchemyEndpoint =`https://arb-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`
