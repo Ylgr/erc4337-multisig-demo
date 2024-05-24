57f00ebce27509e87ca201a33e56ec42adcb8794
viet tu <quyen.luminous@gmail.com>
2024-05-24 21:05:38 +0700

    (wip) setup init wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
 import { useState } from 'react'
-import {AlchemyEndpoint, Safe4337ModuleAddress, SafeSingletonAddress, SafeProxyFactoryAddress} from '/constants';
+import {
+    AlchemyEndpoint,
+    Safe4337ModuleAddress,
+    SafeSingletonAddress,
+    SafeProxyFactoryAddress,
+    SafeSingletonContract, SafeModuleSetupAddress, SafeProxyFactoryContract, Safe4337ModuleContract
+} from '/constants';
 import './App.css'
+import {ethers} from "ethers";
 
 function App() {
+
+    function testCreateSafe() {
+        const wallet = new ethers.Wallet(process.env.REACT_APP_PRIVATE_KEY as string)
+        const testWallet = wallet.address
+        const encodedInitializer = SafeSingletonContract.interface.encodeFunctionData("setup", [
+            [testWallet],
+            1,
+            SafeModuleSetupAddress,
+            SafeSingletonContract.interface.encodeFunctionData('enableModules', [[Safe4337ModuleAddress]]),
+            Safe4337ModuleAddress,
+            ethers.ZeroAddress,
+            0,
+            ethers.ZeroAddress,
+        ]);
+        const initCode = ethers.concat([
+            SafeProxyFactoryAddress,
+            SafeProxyFactoryContract.interface.encodeFunctionData("createProxyWithNonce", [SafeSingletonAddress as string, encodedInitializer, 73]),
+        ]);
+
+        const userOpCallData = Safe4337ModuleContract.interface.encodeFunctionData("execTransaction", [testWallet, 0, "0x"]);
+
+        // Native tokens for the pre-fund 💸
+        await wallet.sendTransaction({ to: deployedAddress, value: hre.ethers.parseEther("0.005") });
+        // The bundler uses a different node, so we need to allow it sometime to sync
+
+        const userOperation = {
+            sender: deployedAddress,
+            nonce: "0x0",
+            initCode,
+            callData: userOpCallData,
+            callGasLimit: "0x1",
+            verificationGasLimit: "0x1",
+            preVerificationGas: "0x1",
+            maxFeePerGas,
+            maxPriorityFeePerGas,
+            paymasterAndData: "0x",
+            signature: "0x",
+        };
+
+
+    }
+
   return (
     <>
+      <button></button>
     </>
   )
 }
--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,11 @@ import SafeSingletonABI from './abi/Safe.json'
 export const Safe4337ModuleAddress = '0xa581c4A4DB7175302464fF3C06380BC3270b4037'
 export const SafeProxyFactoryAddress = '0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67'
 export const SafeSingletonAddress = '0x41675C099F32341bf84BFc5382aF534df5C7461a'
+export const SafeModuleSetupAddress = '0x8EcD4ec46D4D2a6B64fE960B3D64e8B94B2234eb'
+
 export const AlchemyEndpoint =`https://arb-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`
 
-const provider = new ethers.JsonRpcProvider(AlchemyEndpoint)
-const Safe4337ModuleContract = new ethers.Contract(Safe4337ModuleAddress, Safe4337ModuleABI, provider)
-const SafeProxyFactoryContract = new ethers.Contract(SafeProxyFactoryAddress, SafeProxyFactoryABI, provider)
-const SafeSingletonContract = new ethers.Contract(SafeSingletonAddress, SafeSingletonABI, provider)
+export const provider = new ethers.JsonRpcProvider(AlchemyEndpoint)
+export const Safe4337ModuleContract = new ethers.Contract(Safe4337ModuleAddress, Safe4337ModuleABI, provider)
+export const SafeProxyFactoryContract = new ethers.Contract(SafeProxyFactoryAddress, SafeProxyFactoryABI, provider)
+export const SafeSingletonContract = new ethers.Contract(SafeSingletonAddress, SafeSingletonABI, provider)
